020aa4bd6695fc7db37468a33f5ca87d0f7c773c
eya <eya.bouzaiene@esprit.tn>
2021-05-14 00:27:46 +0100

    commit2

--- HomeOfArt-main/frint/view/back/afficherLivraison.php
+++ HomeOfArt-main/frint/view/back/afficherLivraison.php
@@ -16,7 +16,7 @@ $livraisonC=new LivraisonC();
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		
-		<title> Afficher Liste livraison </title>
+		<title> Afficher liste livraison </title>
 		
    
     </head>
